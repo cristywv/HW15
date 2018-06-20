70e278dd0ddb4de757c406031e40197a336fb898
cristywv <38766734+cristywv@users.noreply.github.com>
2018-06-20 18:24:59 -0500

    Add files via upload

---
+++ headliner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class headliner : opener
+    {
+        public string _production { get; set; }
+        public headliner (string name, int hour, int fans, string production): base (name, hour, fans)
+        {
+            _name = name;
+            _hour = hour;
+            _fans = fans;
+            _production = production;
+        }
+       
+    }
+}
---
+++ opener.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class opener
+    {
+        public string _name { get; set; }
+        public int _hour { get; set; }
+        public int _fans { get; set; }
+
+        public opener (string name, int hour, int fans)
+        {
+            _name = name;
+            _hour = hour;
+            _fans = fans;
+        }
+    }
+}
